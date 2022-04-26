07ce2627c181b58070ef72e9457064acdc33fed0
Sebastian Ottow <seb.ottow@gmail.com>
2022-04-26 14:34:03 +0200

    Add mui

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
+import { Container, Grid, Typography } from "@mui/material";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <Link href="/create">Create new Card</Link>
+      <Container maxWidth="xl">
+        <Grid container spacing={4}>
+          <Grid item xs={12}>
+            <Typography variant="h1">Home</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Link href="/create">Create new Card</Link>
+          </Grid>
+          <Grid item xs={12}>
+            <Image
+              src="/School of Fish 3.jpg"
+              alt="School of Fish"
+              width={640}
+              height={426}
+              layout="responsive"
+            />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
